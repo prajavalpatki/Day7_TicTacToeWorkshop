6d27b63df6e639403c96786be838b2dab60e3348
prajavalpatki <prajavalpatki.com>
2022-04-04 09:45:18 +0530

    Choose a letter X or O

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,6 +1,10 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+    static char COMPUTER_OPTION;
+    static char PLAYER_OPTION;
     char [] board = new char[10];
 
     void initialize() {
@@ -9,9 +13,26 @@ public class TicTacToeGame {
         }
     }
 
+    void inputOption() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the option X or O");
+        String input = scan.next();
+        if(input.equals("X")) {
+            PLAYER_OPTION = input.charAt(0);
+            COMPUTER_OPTION = "O".charAt(0);
+        }
+        else {
+            PLAYER_OPTION = input.charAt(0);
+            COMPUTER_OPTION = "X".charAt(0);
+        }
+        System.out.println("Player option : "+ PLAYER_OPTION +  "  Computer Option : " + COMPUTER_OPTION);
+    }
+
     public static void main(String[] args) {
 
         TicTacToeGame game = new TicTacToeGame();
         game.initialize();
+        game.inputOption();
     }
+
 }
